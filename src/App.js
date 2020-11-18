5600faac1e3b46ef9a1d27ae097ee079cdd19f99
Antonio Mesquita <antonio@parafernalia.net.br>
2020-11-17 21:16:44 -0300

    Feat: end to end testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [terms, setTerms] = useState(false)
+  const [test, setTest] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Output:', { name, email, message, terms, test })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="field">
+          <div className="control">
+            <label className="label">Full Name</label>
+            <input name="fullname" type="text" placeholder="Full Name" className="input" value={name} onChange={e => setName(e.target.value)}/>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <label className="label">Email Address</label>
+            <input name="email" type="email" placeholder="Email Address" className="input" value={email} onChange={e => setEmail(e.target.value)}/>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <label className="label">Message</label>
+            <textarea className="textarea" placeholder="Message here" name="message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <label className="checkbox">
+              <input
+                name="terms"
+                type="checkbox"
+                checked={terms}
+                onChange={e => setTerms(e.target.checked)}
+              />
+              I agree to the{" "}
+              <a href="https://google.com">terms and conditions</a>
+            </label>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <label className="label">
+              Do you test your React code?
+            </label>
+            <label className="radio">
+              <input
+                type="radio"
+                name="test"
+                onChange={() => setTest(true)}
+                value="Yes"
+                checked={test}
+              />
+              Yes
+            </label>
+            <label className="radio">
+              <input
+                type="radio"
+                name="test"
+                onChange={() => setTest(false)}
+                value="No"
+                checked={!test}
+              />
+              No
+            </label>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="control">
+            <button type="submit" className="button is-link">Submit</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import faker from 'faker';
+import puppeteer from 'puppeteer'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const person = {
+  name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+  email: faker.internet.email(),
+  phone: faker.phone.phoneNumber(),
+  message: faker.random.words()
+};
+
+describe('End to end test with puppeteer', () => {
+  test('Should be able to fill the form and submit', async () => {
+    let browser = await puppeteer.launch({
+      headless: false,
+      devtools: true,
+      slowMo: 250
+    });
+    let page = await browser.newPage();
+  
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    })
+  
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.contact-form');
+
+    await page.click('input[name=fullname]');
+    await page.type('input[name=fullname]', person.name);
+
+    await page.click('input[name=email]');
+    await page.type('input[name=email]', person.email);
+
+    await page.click('textarea[name=message]');
+    await page.type('textarea[name=message]', person.phone);
+
+    await page.click('input[type=checkbox]');
+
+    await page.click('button[type=submit]');
+  
+    browser.close();
+
+  }, 3000000)
+})
